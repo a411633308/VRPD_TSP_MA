51895d0d1dd61ca69bff58fa60c0fc25d3b343a2
a411633308 <411633308@qq.com>
2021-07-22 00:04:49 +0800

    1. preprocessing of variables and constrains for the Gurobi

--- VRPD_TSP/Classes/methods/GRB_Var_Constrs.py
+++ VRPD_TSP/Classes/methods/GRB_Var_Constrs.py
@@ -8,10 +8,10 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 from Classes.PARAMs import color_list, seed_num, graph_params_list, route_params_list, non_fly_num
-from Classes.PARAMs import customer_num, dockhub_num, drones_num_on_van
+from Classes.PARAMs import customer_num, dockhub_num, drones_num_on_van, rate_load_pack_drone, rate_load_pack_van
 
 
-class Solution:
+class GRB_Var_Constrs:
     def __init__(self, route_params: list = route_params_list, non_fly_num: int = non_fly_num,
                  graph_params: list = graph_params_list,
                  seed_num: int = seed_num, color_list: list = color_list):
@@ -67,6 +67,11 @@ class Solution:
         self.dro_connection_matrix: any
         self.van_connection_matrix: any
         self.nodes_oder = [self.route.nodes_graph[i].index for i in range(len(self.route.nodes_graph))]
+        self.dro_rate_load_matrix: any
+        self.van_rate_load_matrix: any
+
+        # ------------ the number of drone and van that have intersections ---------------
+        self.intersection_numbers: list = list()
 
     def plot_graph_Weight(self, graph_url: str):
         """
@@ -99,10 +104,13 @@ class Solution:
         temp = nx.get_node_attributes(self.route.G, name='non_fly_zone')
 
         for i in self.nodes_oder:
-            self.non_fly_zone.update({i: False})
+            self.non_fly_zone.update({i: 0})
             for j in temp:
                 if i==j:
                     self.non_fly_zone.update({i: temp[j]})
+
+        [self.non_fly_zone.update({i: 0}) if self.non_fly_zone[i]==False else self.non_fly_zone.update({i: 1})
+        for i in self.non_fly_zone]
         return self.non_fly_zone
 
     def demand_constrs(self):
@@ -119,12 +127,12 @@ class Solution:
                     self.customer_demand.update({i: temp[j]})
         return self.customer_demand
 
-    def dro_connection_constrs(self):
-        # nx.attr_matrix(self.route.G, edge_attr='weight')
-        self.dro_connection_matrix = nx.attr_matrix(self.graph, edge_attr='weight', rc_order=self.nodes_oder)
-        # print(self.dro_connection_matrix)
-        print("node order of the matrix: ", self.nodes_oder)
-        return self.dro_connection_matrix
+    # def dro_connection_constrs(self):
+    #     # nx.attr_matrix(self.route.G, edge_attr='weight')
+    #     self.dro_connection_matrix = nx.attr_matrix(self.graph, edge_attr='weight', rc_order=self.nodes_oder)
+    #     # print(self.dro_connection_matrix)
+    #     print("node order of the matrix: ", self.nodes_oder)
+    #     return self.dro_connection_matrix
 
     def weight_matrix_fn(self):
         """
@@ -134,6 +142,23 @@ class Solution:
         self.weight_matrix = nx.to_pandas_adjacency(self.route.G, nodelist=self.graph.nodes())
         return self.weight_matrix
 
+    def load_rate_matrix_fn(self):
+        """
+        the matrix of drones and van to effect the left energy and load weight.
+        :return: the rate of weight and load for the van and the drone
+        """
+        shape = len(self.graph.nodes())
+
+        # generate rate matrix of the load and energy for the drone
+        temp = np.repeat(1-rate_load_pack_drone, shape*shape)
+        self.dro_rate_load_matrix = temp.reshape([shape, shape])
+
+        # generate rate matrix of the load and energy for the drone
+        temp = np.repeat(1-rate_load_pack_van, shape*shape)
+        self.van_rate_load_matrix = temp.reshape([shape, shape])
+
+        return self.dro_rate_load_matrix, self.van_rate_load_matrix
+
     # generate solution matrix
     def adjacency_matrix(self):
         """
@@ -141,40 +166,55 @@ class Solution:
         :return: connection matrix of the nodes
         """
         self.weight_matrix_fn()
-        for i in self.adj_matrix.index:
-            for j in self.adj_matrix.columns:
-                if self.adj_matrix.at[i, j] != 0.0:
+        # initiate the adjacency matrix
+        self.adj_matrix = self.weight_matrix
+        for i in self.weight_matrix.index:
+            for j in self.weight_matrix.columns:
+                if self.weight_matrix.at[i, j] != 0.0:
                     self.adj_matrix.at[i, j] = 1
+        # [print(self.adj_matrix.at[self.adj_matrix.index[i], self.adj_matrix.index[j]] == self.adj_matrix.at[self.adj_matrix.index[j], self.adj_matrix.index[i]])
+        #  for i in range(len(self.adj_matrix)) for j in range(len(self.adj_matrix))]  # whether the matrix is symmetric or not
         return self.adj_matrix
 
-    def generate_solution_matrix_randomly(self):
+    def get_intersection_dro_van(self, solution_matrix_van, solution_matrix_drones):
+        """
+        the intersection nodes that are covered by both drone and van
+        :param solution_matrix_van: the matrix of van
+        :param solution_matrix_drones: the list of matrixes for drones
+        :return: the list of intersection nodes for the drone and van
+        """
+        intersection_matrix: list = list()
+        for n in range(drones_num_on_van):  # there are at least two drones in the network
+            # whether the drone and van have covered the same node
+            c = [1 for i in range(a.shape[0]) for j in range(a.shape[1])
+                 if solution_matrix_van[i, j] == solution_matrix_drones[n][i, j] and a[i,j] == 1]
+            intersection_matrix.append(c)
+
+        # sum the number of intersection nodes between (drone_1 and van, drone_2 and van)
+        [self.intersection_numbers.append(sum(i)) for i in intersection_matrix]
+        return intersection_matrix
+
+    def generate_solution_matrix(self):
         """
         generate the solution matrix of the drones and van
         :return: a list: [solution matrix for the van, solution matrixes for the drones on the van]
         """
         shape = len(self.graph.nodes())
-        self.solution_matrix_van = np.random.randint(0, 2, (shape, shape))
-        [self.solution_matrix_drones.append(np.random.randint(0, 2, (shape, shape))) for i in range(drones_num_on_van)]
-        # print("randomly generated solution matrix for van\n", self.solution_matrix_van)
-        # print("randomly generated solution matrix for drones\n", self.solution_matrix_drones)
+        self.solution_matrix_van = np.zeros([shape, shape])
+        [self.solution_matrix_drones.append(np.zeros([shape, shape])) for i in range(drones_num_on_van)]
+
         return self.solution_matrix_van, self.solution_matrix_drones
 
 
-initial_solution = Solution()
+initial_solution = GRB_Var_Constrs()
 #initial_solution.plot_graph_Weight(r'G:\Unterricht\05-2021\Ipad_Sharing\MA\Routing\8th\map_graph.png')
 # ---initiate the related parameters to generate solution matrices
 # initial_solution.weight_matrix_fn()
-initial_solution.non_fly_zone_constrs()
-# initial_solution.weights_adj_matrix()
 # ------generate matrix randomly
-# solution_van, solution_drones = initial_solution.generate_solution_matrix_randomly()
-
+a, b = initial_solution.generate_solution_matrix()
+adj_matrix = initial_solution.adjacency_matrix()
+# print(adj_matrix.shape, '\n', adj_matrix)
 # initial_solution.weights_adj_matrix()
-# initial_solution.non_fly_zone_constrs()
-# print("The weight of drone matrix: \n", initial_solution.dro_weight_matrix)
-# print("The weight of van matrix: \n", initial_solution.van_weight_matrix)
-# # initial_solution.route.plot_map(r'G:\Unterricht\05-2021\Ipad_Sharing\MA\Routing\8th\randomly_map_graph.png', initial_solution.color_map)
-# #
-# initial_solution.plot_graph_droWeight(r'G:\Unterricht\05-2021\Ipad_Sharing\MA\Routing\8th\randomly_map_graph_dro.png')  # plot the complete graph
-# initial_solution.plot_graph_vanWeight(r'G:\Unterricht\05-2021\Ipad_Sharing\MA\Routing\8th\randomly_map_graph_van.png')
-# # ------whether the solution satisfy the requirements
+initial_solution.non_fly_zone_constrs()
+initial_solution.load_rate_matrix_fn()
+initial_solution.get_intersection_dro_van()
